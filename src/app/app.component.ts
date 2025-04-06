28c66cbc635d91b70bcfb52cf6379500368aa7b4
Brandt Michael <michaelbrandt1@gmx.net>
2025-04-06 14:45:05 +0200

    feat - navigate to disver/popular on default route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { ApiService } from './services/api.service';
+import { Observable } from 'rxjs';
+import { SidebarDesktopComponent } from './components/sidebars/sidebar-desktop/sidebar-desktop.component';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [SidebarDesktopComponent, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  Api = inject(ApiService);
+  router = inject(Router);
   title = 'angular-movie';
+  protected api = inject(ApiService);
+  isLoading$?: Observable<number>;
+
+  ngOnInit(): void {
+    this.router.navigate(['/Discover/Popular']);
+  }
 }
