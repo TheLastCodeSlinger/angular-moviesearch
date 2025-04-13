60b6c734b302217bf4964b110b9f3ead78be9586
Brandt Michael <michaelbrandt1@gmx.net>
2025-04-13 18:10:46 +0200

    feat - update movieitem style

--- src/app/components/movie-item/movie-item.component.ts
+++ src/app/components/movie-item/movie-item.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { MovieBase } from '@/types/api-types';
 import { RouterLink } from '@angular/router';
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-movie-item',
-  imports: [RouterLink],
+  imports: [RouterLink, DecimalPipe],
   templateUrl: './movie-item.component.html',
   standalone: true,
 })
